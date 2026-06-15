c50e15b194cd3e2a52f7f75e87357061ea4b14d7
bagusfhrazm44 <bagusfakhriazzam@gmail.com>
2026-06-15 20:37:11 +0700

    update lagi

--- js/main.js
+++ js/main.js
@@ -115,6 +115,11 @@
         }
     });
 
+    // Active menu saat diklik
+    $('.navbar-nav .nav-link').on('click', function() {
+        $('.navbar-nav .nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
     
 })(jQuery);
 
